6d8c4a90244ddace6b42bac211e15b386dcde2eb
harshamupparaju <harshadiablo@gmail.com>
2024-01-04 19:38:14 +0530

    Working base code without train test splits

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ import matplotlib.pyplot as plt
 import time
 from tempfile import TemporaryDirectory
 import random
+import time
+
+from torchmetrics.classification import MulticlassF1Score, MulticlassRecall
+
 torch.manual_seed(0)
 cudnn.benchmark = True
 plt.ion()   # interactive mode
@@ -74,14 +78,18 @@ class CASME3Dataset(Dataset):
 
         emotion = self.label_mapping[emotion]
 
+
+        print(f'{subject} {sequence} {onset_frame} {apex_frame} {offset_frame} {emotion}')
+
         #Picking 13 frames randomly 
         video_frames = []
+
         while len(video_frames) < 13:
             potential_frame = random.randint(onset_frame+1, offset_frame)
             potential_frame_path = self.data_root / subject / sequence / 'color' / f'{potential_frame}.jpg'
             if (potential_frame not in video_frames) and (potential_frame != apex_frame) and potential_frame_path.is_file():
                 video_frames.append(potential_frame)
-
+        
 
 
         # #Picking 16 frames around apex frame
@@ -95,6 +103,9 @@ class CASME3Dataset(Dataset):
         video_frames.append(apex_frame)
         video_frames.append(offset_frame)
         video_frames.sort()
+
+
+
 
 
         numpy_video_frames = np.array([np.array(Image.open(self.data_root / subject / sequence / 'color' / f'{frame}.jpg')) for frame in video_frames])
@@ -117,6 +128,39 @@ class CASME3Dataset(Dataset):
         self.annotations = self.annotations[
             (self.annotations['Offset'] - self.annotations['Onset'] + 1) >= 16]
 
+        # self.annotations.remove(self.annotations[self.annotations['Subject'] == 'spNO.171'] & self.annotations[self.annotations['Filename'] == 'e'])
+        # self.annotations.drop(self.annotations[self.annotations['Subject'] == 'spNO.171'] & self.annotations[self.annotations['Filename'] == 'e'], inplace=True)
+        a = self.annotations['Subject'] == 'spNO.171'
+        b = self.annotations['Filename'] == 'e'
+        c = self.annotations['Onset'] == 2403
+
+        self.annotations.drop(self.annotations[a & b & c].index, inplace=True)
+
+        a = self.annotations['Subject'] == 'spNO.171'
+        b = self.annotations['Filename'] == 'h'
+        c = self.annotations['Onset'] == 1340
+
+        self.annotations.drop(self.annotations[a & b & c].index, inplace=True)
+
+        a = self.annotations['Subject'] == 'spNO.208'
+        b = self.annotations['Filename'] == 'c'
+        c = self.annotations['Onset'] == 2334
+
+        self.annotations.drop(self.annotations[a & b & c].index, inplace=True)
+
+        a = self.annotations['Subject'] == 'spNO.215'
+        b = self.annotations['Filename'] == 'j'
+        c = self.annotations['Onset'] == 463
+
+        self.annotations.drop(self.annotations[a & b & c].index, inplace=True)
+
+        a = self.annotations['Subject'] == 'spNO.216'
+        b = self.annotations['Filename'] == 'e'
+        c = self.annotations['Onset'] == 0
+
+        self.annotations.drop(self.annotations[a & b & c].index, inplace=True)
+
+        # self.annotations.drop(self.annotations[(self.annotations['Subject'] == 'spNO.171') & (self.annotations[self.annotations['Filename'] == 'e'])].index, axis=0, inplace=True)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
@@ -131,7 +175,14 @@ data_transforms = transforms.Compose([
 #Initialize the dataset
 facial_expression_dataset = CASME3Dataset(data_root, MAE_annotations_path, ME_annotations_path, transform=data_transforms)
 
-dataloader = DataLoader(facial_expression_dataset, batch_size=24, shuffle=False, num_workers=4)
+print(len(facial_expression_dataset))
+print(1/0)
+
+train_size = int(0.8 * len(facial_expression_dataset))
+test_size = len(facial_expression_dataset) - train_size
+train_dataset, test_dataset = torch.utils.data.random_split(facial_expression_dataset, [train_size, test_size])
+
+dataloader = DataLoader(facial_expression_dataset, batch_size=2  , shuffle=False, num_workers=0)
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
@@ -145,7 +196,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
         for epoch in range(num_epochs):
             print(f'Epoch {epoch}/{num_epochs - 1}')
             print('-' * 10)
-
+            
             # Each epoch has a training and validation phase
             for phase in ['train', 'val']:
                 if phase == 'train':
@@ -155,18 +206,22 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
                 running_loss = 0.0
                 running_corrects = 0
-
+                targets = []
+                predictions = []
                 for inputs, labels in dataloader:
                     inputs = inputs.to(device)
                     labels = labels.to(device)
-
                     optimizer.zero_grad()
 
                     # forward
                     # track history if only in train
                     with torch.set_grad_enabled(phase == 'train'):
+                        
                         outputs = model(inputs)
+
                         _, preds = torch.max(outputs, 1)
+                        predictions.append(preds)
+                        targets.append(labels.data)
                         loss = criterion(outputs, labels)
 
                         # backward + optimize only if in training phase
@@ -174,16 +229,29 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                             loss.backward()
                             optimizer.step()
 
+
+
                     # statistics
                     running_loss += loss.item() * inputs.size(0)
                     running_corrects += torch.sum(preds == labels.data)
+
+                predictions = torch.cat(predictions)
+                targets = torch.cat(targets)
+
+                f1 = MulticlassF1Score(num_classes=8, average='macro').to(device)
+                Recall = MulticlassRecall(num_classes=8, average='macro').to(device)
+                unweighted_f1_score = f1(predictions, targets)
+                unweighted_average_recall = Recall(predictions, targets)
+                # unweighted_f1_score = f1_score(predictions, targets, num_classes=8, average='macro')
+                print(unweighted_f1_score)
                 if phase == 'train':
                     scheduler.step()
 
                 epoch_loss = running_loss / len(facial_expression_dataset)
                 epoch_acc = running_corrects.double() / len(facial_expression_dataset)
-
-                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+                epoch_f1_score = unweighted_f1_score
+                epoch_recall = unweighted_average_recall
+                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f} UF1: {epoch_f1_score:.4f} UAR: {epoch_recall:.4f}')
 
                 # deep copy the model
                 if phase == 'val' and epoch_acc > best_acc:
@@ -222,7 +290,7 @@ optimizer = optim.SGD(model.head.parameters(), lr=1e-3, momentum=0.9)
 # Decay LR by a factor of 0.1 every 7 epochs
 exp_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
-model = train_model(model, criterion, optimizer, exp_scheduler, num_epochs=25)
+model = train_model(model, criterion, optimizer, exp_scheduler, num_epochs=1)
 
 
 
